4718d673aa8b25e80fb929fe717c348aaed95057
Shivam <kalrashivam47@gmail.com>
2024-02-26 00:31:10 +0530

    home

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,29 +1,26 @@
 import React from "react";
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon,
-  MDBBtn,
-} from "mdb-react-ui-kit";
+import { MDBFooter, MDBContainer, MDBBtn, MDBIcon } from "mdb-react-ui-kit";
+import { Link } from "react-router-dom"; // Import Link from React Router
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+
 export default function App() {
   return (
     <MDBFooter className="bg-dark text-center text-white">
       <MDBContainer className="p-4 pb-0">
         <section className="mb-4">
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="facebook-f" />
-          </MDBBtn>
+          {/* Use Link instead of a regular anchor tag */}
+          <Link to="https://www.facebook.com/profile.php?id=61556736535454&mibextid=kFxxJD">
+            <MDBBtn
+              outline
+              color="light"
+              floating
+              className="m-1"
+              role="button"
+            >
+              <MDBIcon fab icon="facebook-f" />
+            </MDBBtn>
+          </Link>
         </section>
       </MDBContainer>
 
