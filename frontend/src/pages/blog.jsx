28f795b006b4e9840242e003fbd5eb3248ab7959
Shivam <kalrashivam47@gmail.com>
2024-02-25 17:42:54 +0530

    nl

--- frontend/src/pages/blog.jsx
+++ frontend/src/pages/blog.jsx
@@ -22,10 +22,10 @@ const blog = () => {
           <p>
             Tiffany Debro is the founder and owner of Faith to Faith Solutions
             LLC. She have always had a passion for caring for others, that has
-            resulted in her having over 14 years of medical experience. FFaith
-            to Faith Solutions is a God given idea that came to life during a
-            time where Tiffany experienced homelessness and hardship herself. It
-            was God truly taking her from "Faith to Faith"; but out of hardship
+            resulted in her having over 14 years of medical experience. Faith to
+            Faith Solutions is a God given idea that came to life during a time
+            where Tiffany experienced homelessness and hardship herself. It was
+            God truly taking her from "Faith to Faith"; but out of hardship
             would come a compassion for those who would experience similar
             situations and circumstance. Tiffany have always had a big heart for
             those who tend to be overlooked and forgotten. So providing a safe,
